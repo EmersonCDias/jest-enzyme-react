5a71c9b61f59fa6aeec5a74e681e40803f511488
EmersonCDias <emerson_cdias@hotmail.com>
2021-04-17 12:28:32 -0300

    Adding success code and test in input component

--- jotto/src/components/Input.js
+++ jotto/src/components/Input.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({ secretWord }) => <div data-test="input"></div>
+const Input = ({ success, secretWord }) => {
+  const [currentGuess, setCurrentGuess] = useState("");
 
+  return (
+    <>
+      {!success && (
+        <div data-test="input">
+          <form className="form-inline">
+            <input
+              data-test="input-box"
+              type="text"
+              className="mb-2 mx-sm-3"
+              placeholder="enter guess"
+              value={currentGuess}
+              onChange={({ target }) => setCurrentGuess(target.value)}
+            />
+
+            <button
+              onClick={e => {
+                e.preventDefault();
+                setCurrentGuess("");
+              }}
+              data-test="submit-button"
+              className="btn btn-primary mb-2"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
+    </>
+  )
+}
 Input.propTypes = {
   secretWord: PropTypes.string.isRequired,
+  success:  PropTypes.bool.isRequired,
 }
 
 export default Input;
